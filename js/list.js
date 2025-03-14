4ed6b5d322e0a6231f723e0326ab2df1be6b5974
Sam Weiss <sweiss@cct.ie>
2025-03-14 16:05:17 +0000

    counter button

--- js/list.js
+++ js/list.js
@@ -7,3 +7,14 @@ subheading.addEventListener("mouseover", () => { // Anonymous Function
 subheading.addEventListener("mouseout", () => {
     subheading.setAttribute("class", "green");
 })
+
+let counterButton = document.getElementById("counter-button");
+let count = 0;
+counterButton.addEventListener("click", () => {
+    // console.log(count);
+    // document.getElementById("counter-text").innerText = ++count;
+    // console.log(count);
+    let textNode = document.getElementById("counter-text");
+    let currentValue = textNode.innerText;
+    textNode.innerText = ++currentValue;
+})
